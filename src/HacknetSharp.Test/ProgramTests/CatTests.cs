b22d5990e37df98263683b23fda3ca6b6f32fc7c
v69ar73aqvc <54872398+v69ar73aqvc@users.noreply.github.com>
2022-06-29 00:18:53 -0700

    drop unnecessary fs set in tests

--- src/HacknetSharp.Test/ProgramTests/CatTests.cs
+++ src/HacknetSharp.Test/ProgramTests/CatTests.cs
@@ -8,7 +8,7 @@ public class CatTests : ProgramTestBase
     [Test]
     public void NormalFile_Works()
     {
-        UnixFs().UserName("a_user").Files("text:\"/home/{UserName}/test1.txt\" \"ouf\"").Build().Shell();
+        UserName("a_user").Files("text:\"/home/{UserName}/test1.txt\" \"ouf\"").Build().Shell();
         Assert.That(Run("cat /home/a_user/test1.txt").NextText(), Is.EqualTo("ouf\n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
@@ -32,7 +32,7 @@ public class CatTests : ProgramTestBase
     [Test]
     public void MissingFile_Works()
     {
-        UnixFs().UserName("usao").Build().Shell();
+        UserName("usao").Build().Shell();
         Assert.That(Run("cat /home/usao/abec.txt").NextText(), Is.EqualTo("cat: /home/usao/abec.txt: No such file or directory\n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
@@ -40,7 +40,7 @@ public class CatTests : ProgramTestBase
     [Test]
     public void Protected_FromAdmin_Visible()
     {
-        UnixFs().Admin().Files("text+++:/prot/file \"DATA\"").Build().Shell();
+        Admin().Files("text+++:/prot/file \"DATA\"").Build().Shell();
         Assert.That(Run("cat /prot/file").NextText(), Is.EqualTo("DATA\n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
@@ -48,7 +48,7 @@ public class CatTests : ProgramTestBase
     [Test]
     public void ProtectedFolder_FromRegular_Protected()
     {
-        UnixFs().Unelevated().Files("fold+++:/prot/", "text+++:/prot/file \"DATA\"").Build().Shell();
+        Unelevated().Files("fold+++:/prot/", "text+++:/prot/file \"DATA\"").Build().Shell();
         Assert.That(Run("cat /prot/file").NextText(), Is.EqualTo("cat: /prot/file: No such file or directory\n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
@@ -56,7 +56,7 @@ public class CatTests : ProgramTestBase
     [Test]
     public void ProtectedFile_FromRegular_Protected()
     {
-        UnixFs().Unelevated().Files("fold:/prot/", "text+++:/prot/file \"DATA\"").Build().Shell();
+        Unelevated().Files("fold:/prot/", "text+++:/prot/file \"DATA\"").Build().Shell();
         Assert.That(Run("cat /prot/file").NextText(), Is.EqualTo("cat: /prot/file: Permission denied\n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
@@ -64,7 +64,7 @@ public class CatTests : ProgramTestBase
     [Test]
     public void ProtectedFile2_FromRegular_Protected()
     {
-        UnixFs().Unelevated().Files("text+++:/prot/file \"DATA\"").Build().Shell();
+        Unelevated().Files("text+++:/prot/file \"DATA\"").Build().Shell();
         Assert.That(Run("cat /prot/file").NextText(), Is.EqualTo("cat: /prot/file: Permission denied\n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
--- src/HacknetSharp.Test/ProgramTests/LsTests.cs
+++ src/HacknetSharp.Test/ProgramTests/LsTests.cs
@@ -12,7 +12,7 @@ public class LsTests : ProgramTestBase
     [Test]
     public void ExistingFolder_NoTrailingSlash_Works()
     {
-        UnixFs().Files("fold:/test/bin", "fold:/test/lib", "fold:/test/local", "fold:/test/share").Build().Shell();
+        Files("fold:/test/bin", "fold:/test/lib", "fold:/test/local", "fold:/test/share").Build().Shell();
         Assert.That(Run("ls /test", 16).NextText(), Is.EqualTo("bin   lib   \nlocal share \n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
@@ -20,7 +20,7 @@ public class LsTests : ProgramTestBase
     [Test]
     public void ExistingFolder_WithTrailingSlash_Works()
     {
-        UnixFs().Files("fold:/test/bin", "fold:/test/lib", "fold:/test/local", "fold:/test/share").Build().Shell();
+        Files("fold:/test/bin", "fold:/test/lib", "fold:/test/local", "fold:/test/share").Build().Shell();
         Assert.That(Run("ls /test/", 16).NextText(), Is.EqualTo("bin   lib   \nlocal share \n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
@@ -28,7 +28,7 @@ public class LsTests : ProgramTestBase
     [Test]
     public void MissingFolder_MarkedMissing()
     {
-        UnixFs().Build().Shell();
+        Build().Shell();
         Assert.That(Run("ls /usrs").NextText(), Is.EqualTo("ls: /usrs: No such file or directory\n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
@@ -36,7 +36,7 @@ public class LsTests : ProgramTestBase
     [Test]
     public void ProtectedFolder_FromAdmin_Visible()
     {
-        UnixFs().Admin().Files("text+++:/prot/file \"DATA\"").Build().Shell();
+        Admin().Files("text+++:/prot/file \"DATA\"").Build().Shell();
         Assert.That(Run("ls /prot/").NextText(), Is.EqualTo("file \n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
@@ -44,7 +44,7 @@ public class LsTests : ProgramTestBase
     [Test]
     public void ProtectedFile_FromRegular_Visible()
     {
-        UnixFs().Unelevated().Files("fold:/prot/", "text+++:/prot/file \"DATA\"").Build().Shell();
+        Unelevated().Files("fold:/prot/", "text+++:/prot/file \"DATA\"").Build().Shell();
         Assert.That(Run("ls /prot/").NextText(), Is.EqualTo("file \n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
@@ -52,7 +52,7 @@ public class LsTests : ProgramTestBase
     [Test]
     public void ProtectedFolder_FromRegular_Protected()
     {
-        UnixFs().Unelevated().Files("fold+++:/prot/", "text+++:/prot/file \"DATA\"").Build().Shell();
+        Unelevated().Files("fold+++:/prot/", "text+++:/prot/file \"DATA\"").Build().Shell();
         Assert.That(Run("ls /prot/").NextText(), Is.EqualTo("ls: /prot/: No such file or directory\n"));
         Assert.That(ProcessCount(), Is.EqualTo(0));
     }
